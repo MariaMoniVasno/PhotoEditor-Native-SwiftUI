b5ea2ffc359df4c2823b6ba9987d813c0344ddb4
MariaMoniVasno <mariamonivasno@gmail.comuser.name>
2022-05-11 12:15:29 +0530

    Initial Commit

---
+++ Shared/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  Shared
+//
+//  Created by Trident on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
